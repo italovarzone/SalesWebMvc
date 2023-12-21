1cdb46e69a02a4ddfc6dc68ceb15afc217f38b40
Ítalo <italo.varzah@gmail.com>
2023-12-21 13:49:41 -0300

    Foreign key not null (referential integrity)

---
+++ SalesWebMvc/Migrations/20231221163943_DepartmentsForeignKey.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesWebMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class DepartmentsForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "Seller",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "SalesRecord",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "Seller",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "SalesRecord",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+    }
+}
--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -15,6 +15,7 @@ namespace SalesWebMvc.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department? Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() { }
@@ -32,7 +33,7 @@ namespace SalesWebMvc.Models
         public void AddSales(SalesRecord sr)
         {
             Sales.Add(sr);
-        } 
+        }
 
         public void RemoveSales(SalesRecord sr)
         {
@@ -42,6 +43,6 @@ namespace SalesWebMvc.Models
         public double TotalSales(DateTime initial, DateTime final)
         {
             return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
-        } 
+        }
     }
 }
